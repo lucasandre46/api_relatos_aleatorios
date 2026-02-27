39ffc17ba5fa9703ec8725a615d84ce3050c72ca
BETO <lucasandremiguel46@gmail.com>
2026-02-27 16:58:24 -0300

    atlz_falta_testar_webSockt

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import { PrismaService } from '../prisma.service';
 
 @Module({
-    providers: [EventsGateway],
+    providers: [EventsGateway, PrismaService],
     exports: [EventsGateway]
 })
 export class EventsModule { }
